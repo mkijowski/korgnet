8773a6eb8d13c508a98f3da1455cd8317edffc5f
Matthew <matthewkijowski@gmail.com>
2020-06-05 13:20:34 -0400

    welcome message working

--- korgbot.py
+++ korgbot.py
@@ -6,12 +6,34 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server! My name is korgnet and I try my best to keep things tidy around here.\n'
+        f'Eventually I will learn all sorts of tricks, but for now I just welcome people to my house!\n'
+        f'If I learn a new trick I\'ll brag about it in my instructions channel.\n'
+    )
+
+
 
 client.run(TOKEN)
 
