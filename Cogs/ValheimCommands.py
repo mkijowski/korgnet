674041e1febbf7743ebb9a421a3483fa067d395f
Matthew Kijowski <matthewkijowski@gmail.com>
2021-03-09 15:12:41 -0500

    scripts dir

--- Cogs/ValheimCommands.py
+++ Cogs/ValheimCommands.py
@@ -21,12 +21,12 @@ class ValheimCommands(commands.Cog):
     @commands.has_role('Asgardian')
     async def valheim_restart(ctx):
         #alert Matt
-        await dm(member=ctx.guild.fetch_member(218952310053666816), 'Someone sounded the Gjallarhorn!')
+        await dm(member=ctx.guild.fetch_member(218952310053666816), content='Someone sounded the Gjallarhorn!')
 
         #power on server
         await ctx.send('The mighty beast Korgnarok has fled! The roots of Korggdrasil once again allow passage to Korghalla!')
         await ctx.send('All Korghallan\'s may check the fate of this world with `!gramr`.')
-        command = '/home/ubuntu/git/korgnet/gjallarhorn.sh boot'
+        command = '/home/ubuntu/git/korgnet/scripts/gjallarhorn.sh boot'
         response = subprocess.run(command.split(' '), capture_output=True, text=True).stdout
 
         await log(response)
@@ -39,7 +39,7 @@ class ValheimCommands(commands.Cog):
         await ctx.send('You pull the mighty Gramr from the trunk of the great Barnstokkr, \
             \b\b\bits ring pierces the myst.  If anyone is in Korghalla surely they will need your aid!')
     
-        command = '/home/ubuntu/git/korgnet/gjallarhorn.sh check_status'
+        command = '/home/ubuntu/git/korgnet/scripts/gjallarhorn.sh check_status'
         response = subprocess.run(command.split(' '), capture_output=True, text=True).stdout
     
         await ctx.send(response)
